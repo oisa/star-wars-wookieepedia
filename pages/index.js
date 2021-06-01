0526cd4ef453824a9fe99e97cfc7c6759be00497
oisa <imucup@gmail.com>
2021-06-01 16:12:20 +1000

    Removes unused comments and components.

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,6 @@ import styles from '../styles/Home.module.scss'
 // Components
 import Header from '../components/Header/Header';
 import Footer from '../components/Footer/Footer';
-import Foo from '../components/Foo/Foo.js';
 import FilmList from '../components/FilmList/FilmList';
 import FilmSearch from '../components/FilmSearch/FilmSearch';
 import ThemeComponent from '../components/Theme/ThemeComponent';
